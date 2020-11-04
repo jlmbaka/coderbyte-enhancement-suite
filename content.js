202163b2431c90c5ba055887ff503318d5918bdb
Jean-Louis <jeanlouis.mbaka@gmail.com>
2020-11-04 15:16:11 +0100

    Refactors code and adjusts name

--- content.js
+++ content.js
@@ -1,28 +1,41 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "clicked_browser_action") {
-    // parse the parse in order to extract the data
-    const dashboard = document.querySelectorAll(
-      "li.mainTableHeaderRow, li.candidateRow.candidateRow"
-    );
-    const result = [...dashboard].map((line) => {
-      return [...line.children].map((item) => item.innerText.trim());
-    });
-    console.log(result);
-    // create CSV
-    const csvContent = result
-      .map((item) => JSON.stringify(item))
-      .join("\n")
-      .replace(/(^\[)|(\]$)/gm, "");
-    // configure CSV for download
-    const csvBlob = new Blob([csvContent], {
-      type: "text/csv",
-    });
-    const csvUrl = URL.createObjectURL(csvBlob);
-    // download
-    chrome.runtime.sendMessage({
-      message: "download_csv",
-      url: csvUrl,
-    });
-    // chrome.downloads.download({ url: csvUrl });
+    const testResults = extractTestResultsFromPage();
+    const csvContent = createCsvContent(testResults);
+    const csvUrl = configureCsvForDownload(csvContent);
+    downloadCsv(csvUrl);
   }
 });
+
+function configureCsvForDownload(csvContent) {
+  const csvBlob = new Blob([csvContent], {
+    type: "text/csv",
+  });
+  const csvUrl = URL.createObjectURL(csvBlob);
+  return csvUrl;
+}
+
+function downloadCsv(csvUrl) {
+  chrome.runtime.sendMessage({
+    message: "download_csv",
+    url: csvUrl,
+  });
+}
+
+function createCsvContent(result) {
+  return result
+    .map((item) => JSON.stringify(item))
+    .join("\n")
+    .replace(/(^\[)|(\]$)/gm, "");
+}
+
+function extractTestResultsFromPage() {
+  const dashboard = document.querySelectorAll(
+    "li.mainTableHeaderRow, li.candidateRow.candidateRow"
+  );
+  const result = [...dashboard].map((line) => {
+    return [...line.children].map((item) => item.innerText.trim());
+  });
+  console.log(result);
+  return result;
+}
